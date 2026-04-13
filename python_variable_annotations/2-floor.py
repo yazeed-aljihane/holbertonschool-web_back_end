d9cd5f23f5c224c9e1975cb6b61db49f4867343f
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 09:03:55 +0300

    Add utility function to return the floor of a floating-point number

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define a typed utility function for flooring a floating-point number."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a floating-point number."""
+    return int(n)
