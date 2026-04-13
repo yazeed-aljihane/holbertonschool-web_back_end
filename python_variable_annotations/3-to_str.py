9817c57fd4e90759f54961649248262446716d6b
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 09:05:03 +0300

    Add utility function to convert a floating-point number to a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define a typed utility function for converting a floating-point number to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floating-point number."""
+    return str(n)
