4394f933918a10c7fb38dd699096bd5119c064cd
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 10:32:09 +0300

    Update docstring for element_length function to improve clarity

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Define typed utility function for getting the length of list of strings."""
+"""Define a typed utility function for getting the length of list of strings"""
 from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples containing each sequence and its length."""
     return [(i, len(i)) for i in lst]
