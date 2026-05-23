383c0b118b1ba98a6df8a67724eeff2d68a4bb6e
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-05-23 19:15:15 +0300

    feat: update stdin stream handling and add test script for user input processing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf-8");
-
-process.stdin.on("readable", () => {
-  const input = process.stdin.read();
-
-  if (input !== null) {
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
